b6d9482585242825229d686c644e4478174c39cd
Curtis Mckee <cmckee.dev@gmail.com>
2016-10-03 14:51:58 -0600

    Adding initialize method and variables

--- tru_hearing.rb
+++ tru_hearing.rb
@@ -3,43 +3,47 @@ require 'time'
 
 class TruHearing
 
-    @arg_array = [['9:00', '9:30'],
-                ['9:00', '11:30'],
-                ['10:00', '11:00'],
-                ['2:30', '3:00'],
-                ['2:30', '3:30']]
+  def initialize(appointment_array)
+    @apt_array = self.normalize_times(appointment_array)
+    
+    @schedule = self.normalize_times(
+                [ ['8:30', '9:00'],
+                  ['9:00', '9:30'],
+                  ['9:30', '10:00'],
+                  ['10:00', '10:30'],
+                  ['10:30', '11:00'],
+                  ['1:00', '1:30'],
+                  ['1:30', '2:00'],
+                  ['2:00', '2:30'],
+                  ['2:30', '3:00'],
+                  ['3:00', '3:30'],
+                  ['3:30', '4:00'],
+                  ['4:00','4:30'],
+                  ['4:30','5:00'] ])
 
-    @hours = [['8:30', '9:00'],
-           ['9:00', '9:30'],
-           ['9:30', '10:00'],
-           ['10:00', '10:30'],
-           ['10:30', '11:00'],
-           ['1:00', '1:30'],
-           ['1:30', '2:00'],
-           ['2:00', '2:30'],
-           ['2:30', '3:00'],
-           ['3:00', '3:30'],
-           ['3:30', '4:00'],
-           ['4:00','4:30'],
-           ['4:30','5:00']]
+    @open = Time.parse('8:30am')
+    @close = Time.parse('5:00pm')
+    @lunch = Time.parse('12:00pm')
+  end
 
-    open = Time.parse('8:30am')
-    close = Time.parse('5:00pm')
-  
   def team_avail(array)
     # normalize_times method works in pry, but when called in this method
     # =>  NoMethodError: undefined method `length fo nil:NilClass
     #     from tru_hearing.rb:49:in `normalize_times`
-    schedule = normalize_times(@hours)
-    apts = normalize_times(array)
-    
-    schedule.zip(apts).map do |s, a|
+          # SOLVED: self.normalize was defined while varaible was on the instance   
+
+    # Running into issues with zip
+    # =>  TypeError: wrong argument type NilClass (must respond to :each)
+    #     from tru_hearing.rb:35:in `zip'
+    # Think the issue lies with nil elements on @apts_array side after zip
+
+    @schedule.zip(array).map! do |s, a|
       if s == a
-        schedule - apts
-      elsif s.between?(a[0] && a[1]) == true
-        schedule - s
+        @schedule - array
+      elsif s[0].between?(a[0] && a[1]) == true
+        @schedule - s
       elsif a == nil
-        schedule - apts
+        @schedule - array
       else
         x
       end
@@ -80,6 +84,15 @@ class TruHearing
           # schedule[0][0].between?(apts[0][0], apts[0][1]) => false
   end
 
-  x = TruHearing.new
+  def is_lunch?
+  end
+
+  arg_array = [ ['9:00', '9:30'],
+                ['9:00', '11:30'],
+                ['10:00', '11:00'],
+                ['2:30', '3:00'],
+                ['2:30', '3:30']  ]
+
+  tru = TruHearing.new(arg_array)
   binding.pry
 end
